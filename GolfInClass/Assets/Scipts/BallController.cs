634e3765db18521a7861b5faa755e4fb038c8474
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2023-01-22 12:06:46 +0100

    enfin des trucs qui marchent

--- GolfInClass/Assets/Scipts/BallController.cs
+++ GolfInClass/Assets/Scipts/BallController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class BallController : MonoBehaviour
     private float angle;
     public float LineLenght;
 
+    public Slider PowerSlider;
+    private float powerUPTime;
+    private float power;
+
     void Awake()
     {
         ball = GetComponent<Rigidbody>();
@@ -22,7 +27,7 @@ public class BallController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.Q))
         {
             ChangeAngle(-1);
         }
@@ -30,10 +35,14 @@ public class BallController : MonoBehaviour
         {
             ChangeAngle(1);
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Putt();
+        }         
         if (Input.GetKey(KeyCode.Space))
         {
-
-        }
+            PowerUp();
+        } 
         UpdateLinePosition();
     }
 
@@ -46,5 +55,20 @@ public class BallController : MonoBehaviour
     {
         line.SetPosition(0, transform.position);
         line.SetPosition(1, transform.position + Quaternion.Euler(0, angle, 0) * Vector3.forward * LineLenght);
+    }
+
+    private void Putt()
+    {
+        ball.AddForce(Quaternion.Euler(0, angle, 0) * Vector3.forward * MaxPower * power, ForceMode.Impulse);
+        power = 0;
+        PowerSlider.value = 0;
+        powerUPTime = 0;
+    }
+
+    private void PowerUp()
+    {
+        powerUPTime += Time.deltaTime;
+        power = Mathf.PingPong(powerUPTime, 1);
+        PowerSlider.value = power;
     }
 }
