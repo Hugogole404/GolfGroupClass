01f0f1a191c79ab230ee0f56571edb96e9310fb1
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2023-02-02 17:29:54 +0100

    Couscous tajin

---
+++ GolfInClass/Assets/Scipts/Hugo/ButtonsLevelChoice.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsLevelChoice : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> ButtonList;
+    [SerializeField] private GameObject[] ButtonListeSelection;
+
+    //foreach (GameObject item in ButtonList)
+    //{
+    //    if (GameObject.name == 1)
+    //    {
+            
+    //    }
+    //}
+}
