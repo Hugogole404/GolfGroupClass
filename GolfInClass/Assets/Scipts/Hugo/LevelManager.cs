cc526fd34b13c571ad4649e709183d02b7800451
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2023-02-05 15:14:38 +0100

    ça marche bien maintenant

--- GolfInClass/Assets/Scipts/Hugo/LevelManager.cs
+++ GolfInClass/Assets/Scipts/Hugo/LevelManager.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Serialization;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
     public BallController ball;
+    public TextMeshProUGUI labelPlayerName;
 
+    private int playerIndex;
     private PlayerRecords playerRecords;
+
     private void Start()
     {
-        playerRecords = GameObject.Find("Player Record").GetComponent<PlayerRecords>();
+        playerRecords = GameObject.Find("_PlayerRecords").GetComponent<PlayerRecords>();
+        playerIndex = 0;
+        SetupPlayer();
     }
 
     private void SetupPlayer()
     {
-        
+        ball.SetupBall(playerRecords.playerColours[playerIndex]);
+        labelPlayerName.text = playerRecords.playerList[playerIndex].name;
+    }
+
+    public void NextPlayer(int previousPutts)
+    {
+
     }
 }
