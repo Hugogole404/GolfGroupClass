88e06e31ec077603a8b93ed3f6568d17bd73ff85
NozoSen <yamizomu@gmail.com>
2023-02-07 15:19:22 +0100

    Sons

--- GolfInClass/Assets/Scipts/Autre/AudioManager.cs
+++ GolfInClass/Assets/Scipts/Autre/AudioManager.cs
@@ -20,10 +20,15 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
+    void Start()
+    {
+        Play("Theme");
+    }
 
-    
+
     public void Play (string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
