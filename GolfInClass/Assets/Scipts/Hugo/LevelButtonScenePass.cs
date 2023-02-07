8064435d82fd458b48c584bc7b1eee52f48c8345
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2023-02-07 10:14:46 +0100

    oui mais non

--- GolfInClass/Assets/Scipts/Hugo/LevelButtonScenePass.cs
+++ GolfInClass/Assets/Scipts/Hugo/LevelButtonScenePass.cs
@@ -9,8 +9,16 @@ using TMPro;
 public class LevelButtonScenePass : MonoBehaviour
 {
     [SerializeField] private Button _levelButtonClicked;
+    [SerializeField] private Image ImageFade;
 
     public void OnClickButtonLevel()
+    {
+        _levelButtonClicked.transform.DOComplete();
+        _levelButtonClicked.transform.DOPunchScale(new Vector3(0.2f, 0.2f, 0), 0.3f);
+        ImageFade.DOFade(1, 0.8f).OnComplete(FadeLevelClick);
+    }
+
+    public void FadeLevelClick()
     {
         SceneManager.LoadScene("Level_" + _levelButtonClicked.name);
     }
