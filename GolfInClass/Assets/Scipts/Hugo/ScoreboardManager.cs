d24d414810aab2d08c497eaf716b6bd1345ddf59
Hugogole404 <113097671+Hugogole404@users.noreply.github.com>
2023-02-17 10:51:11 +0100

    modifs et fin des bugs

--- GolfInClass/Assets/Scipts/Hugo/ScoreboardManager.cs
+++ GolfInClass/Assets/Scipts/Hugo/ScoreboardManager.cs
@@ -9,7 +9,7 @@ using DG.Tweening;
 
 public class ScoreboardManager : MonoBehaviour
 {
-    public TextMeshProUGUI names, putts;
+    public TextMeshProUGUI names, putts, total;
     private PlayerRecords playerRecords;
 
     private void Start()
@@ -17,15 +17,18 @@ public class ScoreboardManager : MonoBehaviour
         playerRecords = GameObject.Find("_PlayerRecords").GetComponent<PlayerRecords>();
         names.text = "";
         putts.text = "";
+        total.text = "";
         foreach (var player in playerRecords.GetScoreboardList())
         {
             names.text += player.name + "\n";
+            total.text = "Total : ";
             putts.text += player.totalPutts + "\n";
         }
     }
     private void Update()
     {
         putts.fontSize = names.fontSize;
+        total.fontSize = names.fontSize;
     }
     public void ButtonReturnMenu()
     {
